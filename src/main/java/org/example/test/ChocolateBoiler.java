2f7eb15aafd4a370ca3b1b13480974f8ecfca16b
Seongjun Kwon <tttzkq9336@gmail.com>
2024-07-19 16:45:00 +0900

    REFACTOR: synchronized

--- src/main/java/org/example/test/ChocolateBoiler.java
+++ src/main/java/org/example/test/ChocolateBoiler.java
@@ -14,7 +14,11 @@ public class ChocolateBoiler {
         boiled = false;
     }
 
-    public static ChocolateBoiler getInstance() {
+    // synchronized 키워드로 한 스레드가 메서드 사용을 끝내기 전까지
+    // 다른 스레드는 공유 자원에 접근 못하고 대기.
+    // -> 인스턴스 생성이 겹치지 않음(인스턴스 생성 동기화 문제 해결)
+    // 다만, 인스턴스를 생성한 후에는 synchronized 불필요.
+    public static synchronized ChocolateBoiler getInstance() {
         if (cb == null) {
             cb = new ChocolateBoiler();
         }
