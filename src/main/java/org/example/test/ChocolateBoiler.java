6b24774cdd04ebef71a499c03b18647c61dcd1f7
Seongjun Kwon <tttzkq9336@gmail.com>
2024-07-19 15:57:45 +0900

    CREATE class ChocolateBoiler

---
+++ src/main/java/org/example/test/ChocolateBoiler.java
@@ -0,0 +1,52 @@
+package org.example.test;
+
+public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+
+    /**
+     * 초콜릿 보일러가 비어있을 때만 작동.
+     */
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    /**
+     * 보일러에 우유와 초콜릿을 혼합한 재료 넣음.
+     */
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    /**
+     * 보일러가 차있고 아직 끓지 않는 상태에서만
+     * 재료를 다 끓이면 boiled 플래그를 true로 변경.
+     */
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            // 끓일 재료를 다음 단계로 넘김
+            empty = true;
+        }
+    }
+
+    /**
+     * 재료를 끓임
+     */
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
+}
